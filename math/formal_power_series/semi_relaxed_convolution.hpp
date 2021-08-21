9316b1e593b3e612cf1fb709651e57b4eba9dead
hly1204 <hly1204@qq.com>
2021-08-22 04:42:03 +0800

    add semi_relaxed_conv

---
+++ math/formal_power_series/semi_relaxed_convolution.hpp
@@ -0,0 +1,16 @@
+#ifndef SEMI_RELAXED_CONVOLUTION_HEADER_HPP
+#define SEMI_RELAXED_CONVOLUTION_HEADER_HPP
+
+/**
+ * @brief semi-relaxed convolution
+ * @docs docs/math/formal_power_series/semi_relaxed_convolution.md
+ */
+
+#include <algorithm>
+#include <vector>
+
+#include "radix_2_NTT.hpp"
+
+namespace lib {}
+
+#endif
