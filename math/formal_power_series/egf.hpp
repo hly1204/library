f84368c83956993d1e3d1fdaacce791fee6800a5
hly1204 <hly1204@qq.com>
2021-09-19 22:49:52 +0800

    update

---
+++ math/formal_power_series/egf.hpp
@@ -0,0 +1,16 @@
+#ifndef EGF_HEADER_HPP
+#define EGF_HEADER_HPP
+
+/**
+ * @brief exponential generating function
+ * @docs docs/math/formal_power_series/egf.md
+ */
+
+namespace lib {
+
+template <typename mod_t>
+class ExponentialGeneratingFunction {};
+
+} // namespace lib
+
+#endif
