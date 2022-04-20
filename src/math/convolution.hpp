a8140c6ded64f937e0cb243efb000afafa953711
hly1204 <hly1204@qq.com>
2022-04-20 11:00:46 +0800

    remove unnecessary code

--- src/math/convolution.hpp
+++ src/math/convolution.hpp
@@ -14,15 +14,15 @@ std::vector<ModIntT> convolution(const std::vector<ModIntT> &lhs, std::vector<Mo
   int n = static_cast<int>(lhs.size()), m = static_cast<int>(rhs.size());
   if (n == 0 || m == 0) return std::vector<ModIntT>{};
   if (std::min(n, m) <= 32) {
-    std::vector<ModIntT> res(n + m - 1, ModIntT());
+    std::vector<ModIntT> res(n + m - 1);
     for (int i = 0; i != n; ++i)
       for (int j = 0; j != m; ++j) res[i + j] += lhs[i] * rhs[j];
     return res;
   }
   int len = ntt_len(n + m - 1);
   std::vector<ModIntT> lhs_cpy(len), rhs_cpy(len);
-  std::fill(std::copy_n(lhs.cbegin(), n, lhs_cpy.begin()), lhs_cpy.end(), ModIntT());
-  std::fill(std::copy_n(rhs.cbegin(), m, rhs_cpy.begin()), rhs_cpy.end(), ModIntT());
+  std::copy_n(lhs.cbegin(), n, lhs_cpy.begin());
+  std::copy_n(rhs.cbegin(), m, rhs_cpy.begin());
   dft_n(lhs_cpy.begin(), len), dft_n(rhs_cpy.begin(), len);
   for (int i = 0; i != len; ++i) lhs_cpy[i] *= rhs_cpy[i];
   idft_n(lhs_cpy.begin(), len);
