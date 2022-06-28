68cf3d48bf6b43a65672a4a2cecbfb88356bd8f6
hly1204 <hly1204@qq.com>
2022-06-28 22:07:25 +0800

    add ERROR macro

--- remote_test/yosupo/math/pow_of_formal_power_series.0.test.cpp
+++ remote_test/yosupo/math/pow_of_formal_power_series.0.test.cpp
@@ -1,4 +1,5 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/pow_of_formal_power_series"
+#define ERROR
 
 #include "math/formal_power_series.hpp"
 #include "modint/montgomery_modint.hpp"
@@ -12,11 +13,12 @@ int main() {
 #endif
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  int n, m;
+  int n;
+  long long m; // I don't want to deal with the case that `m` is too large.
   std::cin >> n >> m;
   using mint = lib::mm30<998244353>;
   lib::fps<mint> f([it = std::istream_iterator<mint>(std::cin)](int) mutable { return *it++; });
-  auto iv = f.pow(m);
+  auto iv = f.pow(static_cast<int>(m));
   for (int i = 0; i != n; ++i) std::cout << iv[i] << ' ';
   return 0;
 }
