b616f5144e0d7cd2e62f738cae6c75d05cf8faee
hly1204 <hly1204@qq.com>
2024-04-09 21:00:07 +0800

    add testcase

---
+++ test/convolution_mod.0.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+
+#include "fft.hpp"
+#include "modint.hpp"
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<mint> a(n), b(m);
+    for (int i = 0; i < n; ++i) std::cin >> a[i];
+    for (int i = 0; i < m; ++i) std::cin >> b[i];
+    const auto ab = convolution_fft(a, b);
+    for (int i = 0; i < n + m - 1; ++i) std::cout << ab[i] << ' ';
+    return 0;
+}
