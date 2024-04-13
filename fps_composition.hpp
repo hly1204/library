46bff6c9b317ad86c975de30b876d1f2ff76857e
hly1204 <hly1204@qq.com>
2024-04-14 00:11:59 +0800

    add

--- fps_composition.hpp
+++ fps_composition.hpp
@@ -2,7 +2,9 @@
 
 #include "binomial.hpp"
 #include "fft.hpp"
+#include "fps_basic.hpp"
 #include <algorithm>
+#include <cassert>
 #include <vector>
 
 // returns f(g) mod x^n
@@ -75,4 +77,90 @@ inline std::vector<Tp> composition(const std::vector<Tp> &f, const std::vector<T
     auto res = rec(rec, f, Q, 1, k);
     res.resize(n);
     return res;
+}
+
+// returns [x^k]gf^0, [x^k]gf, ..., [x^k]gf^(n-1)
+// see: https://noshi91.hatenablog.com/entry/2024/03/16/224034
+// noshi91. FPS の合成と逆関数、冪乗の係数列挙 Θ(n (log(n))^2)
+template <typename Tp>
+std::vector<Tp> enum_kth_term_of_power(const std::vector<Tp> &f, const std::vector<Tp> &g, int k,
+                                       int n) {
+    if (k < 0 || n <= 0) return {};
+    if (f.empty()) return std::vector<Tp>(n);
+
+    // [x^k] (g(x) / (-f(x) + y))
+    // R[x]((y^(-1)))
+    std::vector<Tp> P(g), Q(k + 1);
+    P.resize(k + 1);
+    for (int i = 0; i < std::min(k + 1, (int)f.size()); ++i) Q[i] = -f[i];
+
+    int d = 1;
+    for (; k; d *= 2, k /= 2) {
+        const int len = fft_len((d * 2) * ((k + 1) * 2) - 1);
+        std::vector<Tp> dftP(len), dftQ(len);
+        for (int i = 0; i < d; ++i)
+            for (int j = 0; j <= k; ++j) {
+                dftP[i * ((k + 1) * 2) + j] = P[i * (k + 1) + j];
+                dftQ[i * ((k + 1) * 2) + j] = Q[i * (k + 1) + j];
+            }
+        dftQ[d * (k + 1) * 2] = 1;
+        fft(dftP);
+        fft(dftQ);
+
+        P.resize(len / 2);
+        Q.resize(len / 2);
+        if (k & 1) {
+            auto &&root = FftInfo<Tp>::get().inv_root(len / 2);
+            for (int i = 0; i != len; i += 2) {
+                P[i / 2] =
+                    (dftP[i] * dftQ[i + 1] - dftP[i + 1] * dftQ[i]).div_by_2() * root[i >> 1];
+                Q[i / 2] = dftQ[i] * dftQ[i + 1];
+            }
+        } else {
+            for (int i = 0; i < len; i += 2) {
+                P[i / 2] = (dftP[i] * dftQ[i + 1] + dftP[i + 1] * dftQ[i]).div_by_2();
+                Q[i / 2] = dftQ[i] * dftQ[i + 1];
+            }
+        }
+        inv_fft(P);
+        inv_fft(Q);
+        if (d * (k + 1) * 4 >= len) Q[(d * (k + 1) * 4) % len] -= 1;
+
+        for (int i = 1; i < d * 2; ++i)
+            for (int j = 0; j <= k / 2; ++j) {
+                P[i * (k / 2 + 1) + j] = P[i * (k + 1) + j];
+                Q[i * (k / 2 + 1) + j] = Q[i * (k + 1) + j];
+            }
+        P.resize(d * 2 * (k / 2 + 1));
+        Q.resize(d * 2 * (k / 2 + 1));
+    }
+
+    std::vector<Tp> invQ(n + 1);
+    auto &&bin = Binomial<Tp>::get(d + n);
+    Tp ff      = 1;
+    for (int i = 0; i <= n; ++i) invQ[n - i] = bin.binom(d + i - 1, d - 1) * ff, ff *= f[0];
+    auto PinvQ = convolution_fft(P, invQ);
+    PinvQ.erase(PinvQ.begin(), PinvQ.begin() + d);
+    PinvQ.resize(n);
+    std::reverse(PinvQ.begin(), PinvQ.end());
+    return PinvQ;
+}
+
+// returns g s.t. f(g) = g(f) = x mod x^n
+template <typename Tp>
+inline std::vector<Tp> reversion(std::vector<Tp> f, int n) {
+    if (n <= 0 || f.size() < 2) return {};
+    assert(f[1] != 0);
+    const auto if1 = f[1].inv();
+    if (n == 1) return {Tp()};
+    f.resize(n);
+    Tp ff = 1;
+    for (int i = 1; i < n; ++i) f[i] *= ff *= if1;
+    auto a     = enum_kth_term_of_power(f, {Tp(1)}, n - 1, n);
+    auto &&bin = Binomial<Tp>::get(n);
+    for (int i = 1; i < n; ++i) a[i] *= (n - 1) * bin.inv(i);
+    auto b = pow(std::vector(a.rbegin(), a.rend() - 1), Tp(1 - n).inv().val(), n - 1);
+    for (int i = 0; i < n - 1; ++i) b[i] *= if1;
+    b.insert(b.begin(), Tp());
+    return b;
 }
