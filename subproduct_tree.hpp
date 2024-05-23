762c7e7752609cd89865b98a8d567ce751805192
hly1204 <hly1204@qq.com>
2024-05-23 22:40:40 +0800

    add

--- subproduct_tree.hpp
+++ subproduct_tree.hpp
@@ -10,12 +10,17 @@
 template <typename Tp>
 class SubproductTree {
 public:
+    // LV=0   => T[0..S]  = DFT((x-X_0)..(x-X_(N-1))    mod (x^S - 1))
+    //        => T[S..2S] = (x-X_0)..(x-X_(N-1))        mod (x^S + 1)      (* SPECIAL CASE)
+    // LV=1   => T[..]    = DFT((x-X_0)..(x-X_(S/2-1))  mod (x^(S/2) - 1))
+    //        => T[..]    = DFT((x-X_(S/2))..(x-X(N-1)) mod (x^(S/2) - 1)) (* GENERAL CASE)
+    // LV=2.. => ..                                                        (* GENERAL CASE)
     std::vector<Tp> T;
     int N;
     int S;
 
-    SubproductTree(const std::vector<Tp> &X) : N(X.size()), S(std::max(fft_len(N), 2)) {
-        assert(N > 0);
+    SubproductTree(const std::vector<Tp> &X)
+        : N(X.size()), S(N == 0 ? 2 : std::max(fft_len(N), 2)) {
         int LogS = 1;
         while ((1 << LogS) < S) ++LogS;
         T.assign((LogS + 1) * S * 2, 1);
@@ -50,6 +55,9 @@ public:
         return res;
     }
 
+    // see:
+    // [1]: A. Bostan, Grégoire Lecerf, É. Schost. Tellegen's principle into practice.
+    // [2]: D. Bernstein. SCALED REMAINDER TREES.
     std::vector<Tp> evaluation(const std::vector<Tp> &F) const {
         const int degF = degree(F);
         const auto P   = product();
@@ -81,9 +89,9 @@ public:
         return res;
     }
 
-    std::vector<Tp> interpolation(const std::vector<Tp> &Y) {
+    std::vector<Tp> interpolation(const std::vector<Tp> &Y) const {
         assert((int)Y.size() == N);
-        const auto D = evaluation(deriv(product())); // denominator => P(x_i)
+        const auto D = evaluation(deriv(product())); // denominator => P'(x_i)
         std::vector<Tp> res(S * 2);
         for (int i = 0; i < N; ++i) res[i * 2] = res[i * 2 + 1] = Y[i] / D[i];
         int LogS = 1;
@@ -94,6 +102,60 @@ public:
                 auto L = T.begin() + ((lv + 1) * S * 2 + i * len * 2); // left child
                 for (int j = 0; j < len; ++j)
                     C[j] = C[len + j] = C[j] * L[len + j] + C[len + j] * L[j];
+                inv_fft_n(C + len, len);
+                if (lv) {
+                    Tp k         = 1;
+                    const auto t = FftInfo<Tp>::get().root(len).at(len / 2);
+                    for (int j = 0; j < len; ++j) C[len + j] *= k, k *= t;
+                    fft_n(C + len, len);
+                }
+            }
+        }
+        return std::vector(res.begin() + S, res.begin() + S + N);
+    }
+
+    // see:
+    // [1]: A. Bostan, É. Schost. Polynomial evaluation and interpolation on special sets of points.
+    // [2]: noshi91. 転置原理なしで Monomial 基底から Newton 基底への変換.
+    //      https://noshi91.hatenablog.com/entry/2023/05/01/022946
+    std::vector<Tp> monomial_to_newton(const std::vector<Tp> &F) const {
+        const int degF = degree(F);
+        assert(degF < N);
+        const auto P = product();
+        // find x^(-1),...,x^(-N) of F/P in R((x^(-1)))
+        auto res = div(std::vector(F.rend() - (degF + 1), F.rend()),
+                       std::vector(P.rbegin(), P.rend()), degF + 1);
+        std::reverse(res.begin(), res.end());
+        res.resize(S);
+        for (int lv = 0, len = S; (1 << lv) < S; ++lv, len /= 2) {
+            std::vector<Tp> RR(len / 2);
+            for (int i = 0; i < (1 << lv); ++i) {
+                auto C = res.begin() + i * len;                              // current
+                auto R = T.begin() + ((lv + 1) * S * 2 + (i * 2 + 1) * len); // right child
+                std::copy_n(C + len / 2, len / 2, RR.begin());
+                fft_n(C, len);
+                for (int j = 0; j < len; ++j) C[j] *= R[j];
+                inv_fft_n(C, len);
+                const int degR = std::max(std::min((i + 1) * len, N) - ((i * len) + len / 2), 0);
+                std::rotate(C, C + degR, C + (degR + len / 2));
+                std::copy_n(RR.begin(), len / 2, C + len / 2);
+            }
+        }
+        res.resize(N);
+        return res;
+    }
+
+    std::vector<Tp> newton_to_monomial(const std::vector<Tp> &F) const {
+        assert((int)F.size() <= N);
+        std::vector<Tp> res(S * 2);
+        for (int i = 0; i < (int)F.size(); ++i) res[i * 2] = res[i * 2 + 1] = F[i];
+        int LogS = 1;
+        while ((1 << LogS) < S) ++LogS;
+        for (int lv = LogS - 1, len = 2; lv >= 0; --lv, len *= 2) {
+            for (int i = 0; i < (1 << lv); ++i) {
+                auto C = res.begin() + i * len * 2;                    // current
+                auto L = T.begin() + ((lv + 1) * S * 2 + i * len * 2); // left child
+                for (int j = 0; j < len; ++j) C[j] = C[len + j] = C[j] + C[len + j] * L[j];
                 inv_fft_n(C + len, len);
                 if (lv) {
                     Tp k         = 1;
---
+++ test/conversion_from_monomial_basis_to_newton_basis.0.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/conversion_from_monomial_basis_to_newton_basis"
+
+#include "modint.hpp"
+#include "subproduct_tree.hpp"
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    int n;
+    std::cin >> n;
+    std::vector<mint> F(n), X(n);
+    for (int i = 0; i < n; ++i) std::cin >> F[i];
+    for (int i = 0; i < n; ++i) std::cin >> X[i];
+    SubproductTree<mint> T(X);
+    const auto res = T.monomial_to_newton(F);
+    for (int i = 0; i < n; ++i) std::cout << res[i] << ' ';
+    return 0;
+}
