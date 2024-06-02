ea36ba300fe667a4591ea6c6e3f363e8c39e0f30
hly1204 <hly1204@qq.com>
2024-06-02 14:06:11 +0800

    add

--- poly_interpolation.hpp
+++ poly_interpolation.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "binomial.hpp"
 #include <cassert>
 #include <vector>
 
+// returns f
+// x: x_0,...
+// y: f(x_0),...
 template <typename Tp>
 inline std::vector<Tp> lagrange_interpolation_naive(const std::vector<Tp> &x,
                                                     const std::vector<Tp> &y) {
@@ -19,4 +23,21 @@ inline std::vector<Tp> lagrange_interpolation_naive(const std::vector<Tp> &x,
         for (int j = n - 1; j >= 0; --j) f[j] += k * t, k = M[j] + k * x[i];
     }
     return f;
+}
+
+// returns f(c)
+// f: polynomial f -> f[0]=f(0),...
+template <typename Tp>
+inline Tp lagrange_interpolation_iota(const std::vector<Tp> &f, Tp c) {
+    if (f.empty()) return 0;
+    const int n = f.size();
+    auto &&bin  = Binomial<Tp>::get(n);
+    std::vector<Tp> k(n);
+    k[0] = f[0];
+    Tp v = 1;
+    for (int i = 1; i < n; ++i) k[i] = f[i] * (v *= (c - (i - 1)) * bin.inv(i));
+    Tp res = k[n - 1];
+    v      = 1;
+    for (int i = n - 2; i >= 0; --i) res += k[i] * (v *= -(c - (i + 1)) * bin.inv(n - 1 - i));
+    return res;
 }
