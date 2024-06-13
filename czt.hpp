f3194c1bd85dc18c26cabc9e6151f8d99cebb5fd
hly1204 <hly1204@qq.com>
2024-06-13 22:04:48 +0800

    add

--- czt.hpp
+++ czt.hpp
@@ -45,3 +45,42 @@ inline std::vector<Tp> czt(std::vector<Tp> F, Tp c, int n, Tp a = 1) {
     for (int i = 1; i < n; ++i) F[i] *= H[i - 1];
     return F;
 }
+
+// returns f s.t. f(aq^i)=F[i]
+// aq^i != aq^j for all i != j
+// see: https://noshi91.github.io/algorithm-encyclopedia/polynomial-interpolation-geometric
+// noshi91. 標本点が等比数列を成す場合に補間多項式を計算するアルゴリズム.
+template <typename Tp>
+std::vector<Tp> iczt(const std::vector<Tp> &F, Tp q, Tp a = 1) {
+    if (F.empty()) return {};
+    if (a == 0) return {F[0]};
+    const int n = F.size();
+    std::vector<Tp> Q(n), S(n), M(n), D(n);
+    Tp qq = 1;
+    // Q[i]=q^i
+    for (int i = 0; i < n; ++i) Q[i] = qq, qq *= q;
+    // S[i]=prod_(i=1..i)(1-q^i)
+    S[0] = 1;
+    for (int i = 1; i < n; ++i) S[i] = S[i - 1] * (1 - Q[i]);
+    const auto invS = batch_inv(S);
+    const auto Sn   = S[n - 1] * (1 - qq);
+    qq              = 1;
+    // M[i]=qbinom(n,i)*binom(i,2)*(-1)^i
+    for (int i = 1; i < n; ++i) M[n - i] = Sn * invS[i] * invS[n - i] * (qq *= -Q[i - 1]);
+    M[0] = qq * -Q[n - 1]; // in case of q^n=1
+    // D[i]=S[i]*S[n-i-1]*q^(binom(i,2)+i(n-i-1))*(-1)^i
+    D[0] = 1;
+    for (int i = 0; i < n - 1; ++i) D[i + 1] = D[i] * -Q[n - i - 2];
+    for (int i = 0; i < n; ++i) D[i] *= S[i] * S[n - i - 1];
+    // D[i] <- -F[i]/D[i]
+    D = batch_inv(D);
+    for (int i = 0; i < n; ++i) D[i] *= -F[i];
+    auto res = convolution(M, czt(D, q.inv(), n, q.inv()));
+    res.resize(n);
+    if (a != 1) {
+        const auto ia = a.inv();
+        Tp aa         = 1;
+        for (int i = 0; i < n; ++i) res[i] *= aa, aa *= ia;
+    }
+    return res;
+}
---
+++ test/polynomial_interpolation_on_geometric_sequence.0.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/polynomial_interpolation_on_geometric_sequence"
+
+#include "czt.hpp"
+#include "modint.hpp"
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    int n;
+    mint a, r;
+    std::cin >> n >> a >> r;
+    std::vector<mint> F(n);
+    for (int i = 0; i < n; ++i) std::cin >> F[i];
+    const auto res = iczt(F, r, a);
+    for (int i = 0; i < n; ++i) std::cout << res[i] << ' ';
+    return 0;
+}
