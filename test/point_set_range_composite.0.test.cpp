0d87be31721d51d2e29e7cd560527da959f2bac2
hly1204 <hly1204@qq.com>
2024-06-21 22:30:09 +0800

    add

---
+++ test/point_set_range_composite.0.test.cpp
@@ -0,0 +1,54 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include "modint.hpp"
+#include "treap_node_base.hpp"
+#include <array>
+#include <iostream>
+#include <memory>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint           = ModInt<998244353>;
+    using LinearFunction = std::array<mint, 2>;
+    struct TreapNode : TreapNodeBase<TreapNode> {
+        LinearFunction Val, Sum;
+        static LinearFunction composition(const LinearFunction &L, const LinearFunction &R) {
+            return LinearFunction{L[0] + L[1] * R[0], L[1] * R[1]};
+        }
+        void do_update() {
+            Sum = Val;
+            if (left()) Sum = composition(Sum, left()->Sum);
+            if (right()) Sum = composition(right()->Sum, Sum);
+        }
+    };
+    int n, q;
+    std::cin >> n >> q;
+    auto buf        = std::make_unique<TreapNode[]>(n);
+    TreapNode *root = nullptr;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> buf[i].Val[1] >> buf[i].Val[0];
+        buf[i].Sum = buf[i].Val;
+        root       = TreapNode::join(root, &buf[i]);
+    }
+    while (q--) {
+        int cmd;
+        std::cin >> cmd;
+        if (cmd == 0) {
+            int p;
+            mint c, d;
+            std::cin >> p;
+            auto node = root->select(p);
+            std::cin >> node->Val[1] >> node->Val[0];
+            node->do_update();
+        } else {
+            int l, r;
+            mint x;
+            std::cin >> l >> r >> x;
+            auto [a, b, c] = TreapNode::split(root, l, r - l);
+            std::cout << TreapNode::composition(b->Sum, {x, 0}).at(0) << '\n';
+            root = TreapNode::join(a, b, c);
+        }
+    }
+    return 0;
+}
