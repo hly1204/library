d398298b0da92af652a423e1fb1b6b262e0fbf3e
hly1204 <hly1204@qq.com>
2024-06-22 17:12:35 +0800

    add

---
+++ test/data_structure/range_affine_range_sum.0.test.cpp
@@ -0,0 +1,56 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include "modint.hpp"
+#include "treap_node_base.hpp"
+#include <iostream>
+#include <memory>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    struct TreapNode : TreapNodeBase<TreapNode> {
+        mint Val, Sum, Add, Mul = {1};
+        void do_propagate() {
+            if (left()) {
+                left()->Add = Mul * left()->Add + Add;
+                left()->Mul *= Mul;
+                left()->Sum = Add * left()->size() + Mul * left()->Sum;
+            }
+            if (right()) {
+                right()->Add = Mul * right()->Add + Add;
+                right()->Mul *= Mul;
+                right()->Sum = Add * right()->size() + Mul * right()->Sum;
+            }
+            Val = Add + Mul * Val;
+            Add = 0;
+            Mul = 1;
+        }
+        void do_update() {
+            Sum = Val;
+            if (left()) Sum += left()->Sum;
+            if (right()) Sum += right()->Sum;
+        }
+    };
+    int n, q;
+    std::cin >> n >> q;
+    auto buf        = std::make_unique<TreapNode[]>(n);
+    TreapNode *root = nullptr;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> buf[i].Val;
+        buf[i].Sum = buf[i].Val;
+        root       = TreapNode::join(root, &buf[i]);
+    }
+    while (q--) {
+        int cmd, l, r;
+        std::cin >> cmd >> l >> r;
+        auto [R0, R1, R2] = TreapNode::split(root, l, r - l);
+        if (cmd == 0) {
+            std::cin >> R1->Mul >> R1->Add;
+        } else {
+            std::cout << R1->Sum << '\n';
+        }
+        root = TreapNode::join(R0, R1, R2);
+    }
+    return 0;
+}
