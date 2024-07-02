139174bacab29ab7c042e0393a06e50029f33f5f
hly1204 <hly1204@qq.com>
2024-07-02 22:15:07 +0800

    add

--- test/matrix/characteristic_polynomial.1.test.cpp
+++ test/matrix/characteristic_polynomial.1.test.cpp
@@ -1,10 +1,10 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/characteristic_polynomial"
 
 #include "basis.hpp"
-#include "fft.hpp"
 #include "mat_basic.hpp"
 #include "modint.hpp"
 #include "random.hpp"
+#include "sbpoly.hpp"
 #include <iostream>
 
 int main() {
@@ -17,14 +17,14 @@ int main() {
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j) std::cin >> A[i][j];
     Basis<mint> B(n);
-    std::vector<mint> cp = {1};
+    SBPoly<mint> cp = {1};
     while (B.size() < n) {
         int deg = 0;
         for (auto R = random_vector<mint>(n);; R = mat_apply(A, R)) {
             if (auto comb = B.insert(R)) {
-                std::vector p(comb->begin() + (B.size() - deg), comb->begin() + B.size());
+                SBPoly<mint> p(comb->begin() + (B.size() - deg), comb->begin() + B.size());
                 p.emplace_back(1);
-                cp = convolution(cp, p);
+                cp *= p;
                 break;
             }
             ++deg;
