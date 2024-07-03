509f2622a0eb47459930219d788cb3faf3e2189f
hly1204 <hly1204@qq.com>
2024-07-03 19:18:33 +0800

    add

---
+++ test/matrix/pow_of_matrix.0.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_matrix"
+
+#include "frobenius.hpp"
+#include "mat_basic.hpp"
+#include "modint.hpp"
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    int n;
+    long long k;
+    std::cin >> n >> k;
+    Matrix<mint> A(n, std::vector<mint>(n));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) std::cin >> A[i][j];
+    Frobenius<mint> F(A);
+    const auto res = F.pow(k);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) std::cout << res[i][j] << " \n"[j == n - 1];
+    return 0;
+}
