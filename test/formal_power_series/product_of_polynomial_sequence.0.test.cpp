aff39f98c2b561b5e946d76fbbebc7e5696eda98
hly1204 <hly1204@qq.com>
2024-07-07 13:18:46 +0800

    add

---
+++ test/formal_power_series/product_of_polynomial_sequence.0.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/product_of_polynomial_sequence"
+
+#include "modint.hpp"
+#include "poly_product.hpp"
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    int n;
+    std::cin >> n;
+    std::vector<std::vector<mint>> L;
+    while (n--) {
+        int d;
+        std::cin >> d;
+        auto &&p = L.emplace_back(d + 1);
+        for (int i = 0; i <= d; ++i) std::cin >> p[i];
+    }
+    const auto res = poly_product(L);
+    for (int i = 0; i < (int)res.size(); ++i) std::cout << res[i] << ' ';
+    return 0;
+}
