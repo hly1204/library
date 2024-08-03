bad936efa4ac697ceebfecbb8c27d8b1b450359f
hly1204 <hly1204@qq.com>
2024-08-03 13:26:56 +0800

    minor

--- poly_basic.hpp
+++ poly_basic.hpp
@@ -40,9 +40,58 @@ inline std::vector<Tp> taylor_shift(std::vector<Tp> a, Tp c) {
 }
 
 // returns (quotient, remainder)
+// O(deg(Q)deg(B))
+template <typename Tp>
+inline std::pair<std::vector<Tp>, std::vector<Tp>> euclidean_div_naive(const std::vector<Tp> &A,
+                                                                       const std::vector<Tp> &B) {
+    const int degA = degree(A);
+    const int degB = degree(B);
+    assert(degB >= 0);
+    const int degQ = degA - degB;
+    if (degQ < 0) return {std::vector<Tp>{Tp(0)}, A};
+    std::vector<Tp> Q(degQ + 1), R = A;
+    const auto inv = B[degB].inv();
+    for (int i = degQ, n = degA; i >= 0; --i)
+        if ((Q[i] = R[n--] * inv) != 0)
+            for (int j = 0; j <= degB; ++j) R[i + j] -= Q[i] * B[j];
+    R.resize(degB);
+    return {Q, R};
+}
+
+// O(min(deg(Q)^2,deg(Q)deg(B)))
+template <typename Tp>
+inline std::vector<Tp> euclidean_div_quotient_naive(const std::vector<Tp> &A,
+                                                    const std::vector<Tp> &B) {
+    const int degA = degree(A);
+    const int degB = degree(B);
+    assert(degB >= 0);
+    const int degQ = degA - degB;
+    if (degQ < 0) return {Tp(0)};
+    const auto inv = B[degB].inv();
+    std::vector<Tp> Q(degQ + 1);
+    for (int i = 0; i <= degQ; ++i) {
+        for (int j = 1; j <= std::min(i, degB); ++j) Q[degQ - i] += B[degB - j] * Q[degQ - i + j];
+        Q[degQ - i] = A[degA - i] - Q[degQ - i] * inv;
+    }
+    return Q;
+}
+
+// returns (quotient, remainder)
 template <typename Tp>
 inline std::pair<std::vector<Tp>, std::vector<Tp>> euclidean_div(const std::vector<Tp> &A,
                                                                  const std::vector<Tp> &B) {
+    const int degA = degree(A);
+    const int degB = degree(B);
+    assert(degB >= 0);
+    // A = Q*B + R => A/B = Q + R/B in R((x^(-1)))
+    const int degQ = degA - degB;
+    if (degQ < 0) return {std::vector<Tp>{Tp(0)}, A};
+    if (degQ < 60 || degB < 60) return euclidean_div_naive(A, B);
+
+    auto Q = div(std::vector(A.rend() - (degA + 1), A.rend()),
+                 std::vector(B.rend() - (degB + 1), B.rend()), degQ + 1);
+    std::reverse(Q.begin(), Q.end());
+
     // returns a mod (x^n-1)
     auto make_cyclic = [](const std::vector<Tp> &a, int n) {
         assert((n & (n - 1)) == 0);
@@ -50,17 +99,6 @@ inline std::pair<std::vector<Tp>, std::vector<Tp>> euclidean_div(const std::vect
         for (int i = 0; i < (int)a.size(); ++i) b[i & (n - 1)] += a[i];
         return b;
     };
-
-    const int degA = degree(A);
-    const int degB = degree(B);
-    assert(degB >= 0);
-    // A = Q*B + R => A/B = Q + R/B in R((x^(-1)))
-    const int degQ = degA - degB;
-    if (degQ < 0) return {std::vector<Tp>{Tp(0)}, A};
-
-    auto Q = div(std::vector(A.rend() - (degA + 1), A.rend()),
-                 std::vector(B.rend() - (degB + 1), B.rend()), degQ + 1);
-    std::reverse(Q.begin(), Q.end());
 
     const int len      = fft_len(std::max(degB, 1));
     const auto cyclicA = make_cyclic(A, len);
@@ -86,6 +124,7 @@ inline std::vector<Tp> euclidean_div_quotient(const std::vector<Tp> &A, const st
     // A = Q*B + R => A/B = Q + R/B in R((x^(-1)))
     const int degQ = degA - degB;
     if (degQ < 0) return {Tp(0)};
+    if (std::min(degQ, degB) < 60) return euclidean_div_quotient_naive(A, B);
 
     auto Q = div(std::vector(A.rend() - (degA + 1), A.rend()),
                  std::vector(B.rend() - (degB + 1), B.rend()), degQ + 1);
