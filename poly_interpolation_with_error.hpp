66d312c10733a4b8bd631bc3517d9235f8316d91
hly1204 <hly1204@qq.com>
2024-08-15 19:30:12 +0800

    add

---
+++ poly_interpolation_with_error.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "poly.hpp"
+#include "subproduct_tree.hpp"
+#include <vector>
+
+// returns F s.t. n=X.size(), deg(F)<n-2t with at most t Y[i] corrupted for F(X[i])=Y[i]
+template <typename Tp>
+inline std::vector<Tp> interpolation_with_error(const std::vector<Tp> &X, const std::vector<Tp> &Y,
+                                                int t) {
+    assert(X.size() == Y.size());
+    const SubproductTree<Tp> T(X);
+    const auto prod   = T.product();
+    const auto B      = T.interpolation(Y);
+    const auto [P, Q] = rational_approximation(Poly<Tp>(B.begin(), B.end()),
+                                               Poly<Tp>(prod.begin(), prod.end()), t * 2);
+    return Poly<Tp>(B.begin(), B.end()) - (P * Poly<Tp>(prod.begin(), prod.end())) / Q;
+}
