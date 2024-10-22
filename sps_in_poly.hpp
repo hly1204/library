6c3ce6ebefdb464aef2187e8663f50c4110e9051
hly1204 <hly1204@qq.com>
2024-10-22 23:05:35 +0800

    add

---
+++ sps_in_poly.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "subset_conv.hpp"
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+// returns F(G)
+// requires deg(F)<=n, G(0)=0
+template <typename Tp>
+inline std::vector<Tp> sps_in_egf(const std::vector<Tp> &F, const std::vector<Tp> &G) {
+    const int N = (int)F.size() - 1;
+    assert((int)G.size() == (1 << N));
+    assert(G[0] == 0);
+
+    std::vector res = {F[N]};
+    for (int i = 0; i < N; ++i) {
+        std::vector<Tp> R(1 << (i + 1));
+        R[0] = F[N - (i + 1)];
+        for (int j = 0; j <= i; ++j) {
+            const auto FG =
+                subset_convolution(std::vector(res.begin(), res.begin() + (1 << j)),
+                                   std::vector(G.begin() + (1 << j), G.begin() + (2 << j)));
+            std::copy(FG.begin(), FG.end(), R.begin() + (1 << j));
+        }
+        R.swap(res);
+    }
+
+    return res;
+}
+
+template <typename Tp>
+inline std::vector<Tp> sps_in_poly(std::vector<Tp> F, std::vector<Tp> G) {
+    const int N = G.size();
+    int LogN    = 0;
+    while ((1 << LogN) != N) ++LogN;
+
+    if (G[0] != 0) {
+        std::vector<Tp> bin(LogN + 1), pw(F.size() + 1), FF(LogN + 1);
+        pw[0] = 1;
+        for (int i = 1; i < (int)pw.size(); ++i) pw[i] = pw[i - 1] * G[0];
+        G[0]   = 0;
+        bin[0] = 1;
+        for (int i = 0; i < (int)F.size(); ++i) {
+            for (int j = 0; j <= std::min(LogN, i); ++j) FF[j] += F[i] * bin[j] * pw[i - j];
+            for (int j = LogN; j > 0; --j) bin[j] += bin[j - 1];
+        }
+        FF.swap(F);
+    }
+
+    F.resize(LogN + 1);
+    Tp c = 1;                                       // factorial
+    for (int i = 1; i <= LogN; ++i) F[i] *= c *= i; // to EGF
+    return sps_in_egf(F, G);
+}
---
+++ test/set_power_series/polynomial_composite_set_power_series.0.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/polynomial_composite_set_power_series"
+
+#include "modint.hpp"
+#include "sps_in_poly.hpp"
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    using mint = ModInt<998244353>;
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<mint> F(n), G(1 << m);
+    for (int i = 0; i < n; ++i) std::cin >> F[i];
+    for (int i = 0; i < (1 << m); ++i) std::cin >> G[i];
+    const auto FG = sps_in_poly(F, G);
+    for (int i = 0; i < (1 << m); ++i) std::cout << FG[i] << ' ';
+    return 0;
+}
