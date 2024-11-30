129effea014fdab6e29629c62dfb0087f12ed016
hly1204 <hly1204@qq.com>
2024-11-30 21:12:38 +0800

    remove unnecessary function

--- fft_doubling.hpp
+++ fft_doubling.hpp
@@ -24,18 +24,3 @@ inline void fft_doubling(std::vector<Tp> &a) {
     a.resize(n * 2);
     fft_doubling_n(a.begin(), n);
 }
-
-template <typename Tp>
-inline std::vector<Tp> fft_doubling2(std::vector<Tp> &a) {
-    const int n = a.size();
-    assert((n & (n - 1)) == 0);
-    a.resize(n * 2);
-    std::copy_n(a.begin(), n, a.begin() + n);
-    inv_fft_n(a.begin() + n, n);
-    const std::vector<Tp> b(a.begin() + n, a.end());
-    Tp k         = 1;
-    const auto t = FftInfo<Tp>::get().root(n).at(n / 2);
-    for (int i = 0; i < n; ++i) a[i + n] *= k, k *= t;
-    fft_n(a.begin() + n, n);
-    return b;
-}
