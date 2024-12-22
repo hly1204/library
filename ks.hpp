b17129ae45d45a8b7771338c5bd2fcf1cdbc210a
hly1204 <hly1204@qq.com>
2024-12-22 21:43:44 +0800

    add example

--- ks.hpp
+++ ks.hpp
@@ -96,6 +96,27 @@ convolution_2d_ks_reciprocal(const std::vector<std::vector<Tp>> &a,
     auto ab1 =
         convolution(pack_2d_ks(make_reciprocal(a, N), N), pack_2d_ks(make_reciprocal(b, N), N));
     std::vector<std::vector<Tp>> ab(a.size() + b.size() - 1, std::vector<Tp>(N * 2 - 1));
+
+    /*
+        example:
+                 let pack: a(x,y) |-> a(x,x^3)
+                      rec: a(x,y) |-> x^2 a(x^(-1),y)
+
+                 a = 1 + 2*x + 4*x^2 + (3 + 4*x)*y
+                 b = 2 + 4*x + 5*x^2 + (5 + 8*x)*y
+           pack(a) = 1 + 2*x + 4*x^2 + 3*x^3 + 4*x^4
+           pack(b) = 2 + 4*x + 5*x^2 + 5*x^3 + 8*x^4
+      pack(rec(a)) = 4 + 2*x +   x^2 + (0 + 4*x + 3*x^2)*y
+      pack(rec(b)) = 5 + 4*x + 2*x^2 + (0 + 8*x + 5*x^2)*y
+                ab = 2 + 8*x + 21*x^2 + 26*x^3 + 20*x^4 +
+                                       (11     + 38*x   + 67*x^2 +  52*x^3)*y +
+                                                                 + (15        + 44*x   + 32*x^2)*y^2
+    pack(a)pack(b) = 2 + 8*x + 21*x^2 + 37*x^3 + 58*x^4 + 67*x^5 +  67*x^6    + 44*x^7 + 32*x^8
+    pack(rec(a))pack(rec(b))
+                   = 20 + 26*x + 21*x^2 + 8*x^3 + 2*x^4 +
+                                        + ... (overlap)
+    */
+
     // restore ab[0]
     for (int i = 0; i < N; ++i) ab[0][i] = ab0[i];
     // ab1[0] = [x^0](x^(2N - 2) a(x^(-1), x^N) b(x^(-1), x^N))
@@ -127,7 +148,7 @@ convolution_2d_ks_negated(const std::vector<std::vector<Tp>> &a,
     if (lenA == 0 || lenB == 0) return std::vector<std::vector<Tp>>(a.size() + b.size() - 1);
     const int N = std::max(lenA, lenB);
     // ab0 = a(x, x^N) b(x, x^N)
-    auto ab0 = convolution(pack_2d_ks(a, N), pack_2d_ks(b, N));
+    const auto ab0 = convolution(pack_2d_ks(a, N), pack_2d_ks(b, N));
 
     // returns a(x, -y)
     auto make_negated = [](const std::vector<std::vector<Tp>> &a) {
@@ -138,7 +159,27 @@ convolution_2d_ks_negated(const std::vector<std::vector<Tp>> &a,
     };
 
     // ab1 = a(x, -x^N) b(x, -x^N)
-    auto ab1 = convolution(pack_2d_ks(make_negated(a), N), pack_2d_ks(make_negated(b), N));
+    const auto ab1 = convolution(pack_2d_ks(make_negated(a), N), pack_2d_ks(make_negated(b), N));
+
+    /*
+        example:
+                 let pack: a(x,y) |-> a(x,x^3)
+
+                 a = 1 + 2*x + 4*x^2 + (3 + 4*x)*y
+                 b = 2 + 4*x + 5*x^2 + (5 + 8*x)*y
+           pack(a) = 1 + 2*x + 4*x^2 + 3*x^3 + 4*x^4
+           pack(b) = 2 + 4*x + 5*x^2 + 5*x^3 + 8*x^4
+     pack(a(x,-y)) = 1 + 2*x + 4*x^2 + -(3*x^3 + 4*x^4)
+     pack(b(x,-y)) = 2 + 4*x + 5*x^2 + -(5*x^3 + 8*x^4)
+                ab = 2 + 8*x + 21*x^2 + 26*x^3 + 20*x^4 +
+                                       (11     + 38*x   + 67*x^2 +  52*x^3)*y +
+                                                                 + (15        + 44*x   + 32*x^2)*y^2
+    pack(a)pack(b) = 2 + 8*x + 21*x^2 + 37*x^3 + 58*x^4 + 67*x^5 +  67*x^6    + 44*x^7 + 32*x^8
+                                       (26+11)   (20+38) ...
+    pack(a(x,-y))pack(b(x,-y))
+                   = 2 + 8*x + 21*x^2 + 15*x^3 + -18*x^4 + ...
+                                       (26-11)   (20-38)
+    */
 
     std::vector<std::vector<Tp>> ab(a.size() + b.size() - 1, std::vector<Tp>(lenA + lenB - 1));
     for (int i = 0; i < (int)(a.size() + b.size() - 1); ++i) {
