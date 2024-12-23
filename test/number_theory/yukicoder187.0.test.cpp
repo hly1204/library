e0fe630eab56e7f6b18e40a354346cabfad4d92e
hly1204 <hly1204@qq.com>
2024-12-23 20:34:50 +0800

    fix

--- test/number_theory/yukicoder187.0.test.cpp
+++ test/number_theory/yukicoder187.0.test.cpp
@@ -14,9 +14,9 @@ int main() {
     for (int i = 0; i < n; ++i) std::cin >> rem[i] >> mod[i];
     if (const auto res = chinese_remainder_mod(rem, mod, 1000000007)) {
         if (std::all_of(rem.begin(), rem.end(), [](int n) { return n == 0; })) {
-            std::cout << std::get<0>(*res) << '\n';
-        } else {
             std::cout << std::get<1>(*res) << '\n';
+        } else {
+            std::cout << std::get<0>(*res) << '\n';
         }
     } else {
         std::cout << "-1\n";
