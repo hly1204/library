9de98b801f9c62721e66ae521e1f33cd7ab50d98
hly1204 <hly1204@qq.com>
2025-01-04 19:35:46 +0800

    add

---
+++ sqrt_int.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cassert>
+#include <type_traits>
+
+// see:
+// [1]: Richard P. Brent and Paul Zimmermann. Modern Computer Arithmetic.
+
+// returns floor(m^(1/2))
+template <typename Int>
+inline std::enable_if_t<std::is_integral_v<Int>, Int> sqrt_int(Int m) {
+    assert(m >= 0);
+    if (m == 0) return 0;
+    for (Int u = m;;) {
+        std::add_const_t<Int> s = u;
+        u                       = (s + m / s) / 2;
+        if (u >= s) return s;
+    }
+}
