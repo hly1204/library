899806ddefbe95228a003375ca180877f441b586
hly1204 <hly1204@qq.com>
2025-01-04 21:32:29 +0800

    minor

--- shift_sample_points.hpp
+++ shift_sample_points.hpp
@@ -26,13 +26,13 @@ inline std::vector<Tp> shift_sample_points(const std::vector<Tp> &f, Tp c, int m
         G[i] = (v == 0) ? Tp(1) : v;
     }
     auto res = middle_product(batch_inv(G), F);
-    SWAG<Tp, std::multiplies<>> prod(std::multiplies<>{});
-    // prod[c-n+1, ..., c]
-    for (int i = -n + 1; i <= 0; ++i) prod.push_back(c + i);
+    SWAG<Tp, std::multiplies<>> P(std::multiplies<>{});
+    // P = c!/(c-n)! = prod[-n < i <= 0](c + i)
+    for (int i = -n + 1; i <= 0; ++i) P.push_back(c + i);
     // res[i] <- (c+i)!/(c+i-n)! * res[i]
     for (int i = 0; i < m; ++i) {
-        if (i) prod.pop_front(), prod.push_back(c + i);
-        const auto v = prod.query().value();
+        if (i) P.pop_front(), P.push_back(c + i);
+        const auto v = P.query().value();
         // 0 <= c+i < n iff (c+i)!/(c+i-n)! = 0
         res[i] = (v == 0) ? f[(c + i).val()] : v * res[i];
     }
