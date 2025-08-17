1ee0140a0573bdecde7ce00df2841e9047d11722
hly1204 <hly1204@qq.com>
2025-08-17 18:27:36 +0800

    add

---
+++ test/formal_power_series/composition_of_formal_power_series_large.1.test.cpp
@@ -0,0 +1,148 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/composition_of_formal_power_series_large"
+
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using uint         = unsigned;
+using ull          = unsigned long long;
+constexpr uint MOD = 998244353;
+
+constexpr uint PowMod(uint a, ull e) {
+    for (uint res = 1;; a = (ull)a * a % MOD) {
+        if (e & 1) res = (ull)res * a % MOD;
+        if ((e /= 2) == 0) return res;
+    }
+}
+
+constexpr uint InvMod(uint a) { return PowMod(a, MOD - 2); }
+
+constexpr uint QUAD_NONRESIDUE = 3;
+constexpr int LOG2_ORD         = __builtin_ctz(MOD - 1);
+constexpr uint ZETA            = PowMod(QUAD_NONRESIDUE, (MOD - 1) >> LOG2_ORD);
+constexpr uint INV_ZETA        = InvMod(ZETA);
+
+std::pair<std::vector<uint>, std::vector<uint>> GetFFTRoot(int n) {
+    assert((n & (n - 1)) == 0);
+    if (n / 2 == 0) return {};
+    std::vector<uint> root(n / 2), inv_root(n / 2);
+    root[0] = inv_root[0] = 1;
+    for (int i = 0; (1 << i) < n / 2; ++i)
+        root[1 << i]               = PowMod(ZETA, 1LL << (LOG2_ORD - i - 2)),
+                  inv_root[1 << i] = PowMod(INV_ZETA, 1LL << (LOG2_ORD - i - 2));
+    for (int i = 1; i < n / 2; ++i)
+        root[i]     = (ull)root[i - (i & (i - 1))] * root[i & (i - 1)] % MOD,
+        inv_root[i] = (ull)inv_root[i - (i & (i - 1))] * inv_root[i & (i - 1)] % MOD;
+    return {root, inv_root};
+}
+
+void Butterfly(uint a[], int n, const uint root[]) {
+    for (int i = n; i >= 2; i /= 2) {
+        for (int k = 0; k < i / 2; ++k) {
+            const uint u = a[k];
+            if ((a[k] += a[k + i / 2]) >= MOD) a[k] -= MOD;
+            if ((a[k + i / 2] = u + MOD - a[k + i / 2]) >= MOD) a[k + i / 2] -= MOD;
+        }
+        for (int j = i, m = 1; j < n; j += i, ++m)
+            for (int k = j; k < j + i / 2; ++k) {
+                const uint u = a[k];
+                a[k + i / 2] = (ull)a[k + i / 2] * root[m] % MOD;
+                if ((a[k] += a[k + i / 2]) >= MOD) a[k] -= MOD;
+                if ((a[k + i / 2] = u + MOD - a[k + i / 2]) >= MOD) a[k + i / 2] -= MOD;
+            }
+    }
+}
+
+void InvButterfly(uint a[], int n, const uint root[]) {
+    for (int i = 2; i <= n; i *= 2) {
+        for (int k = 0; k < i / 2; ++k) {
+            const uint u = a[k];
+            if ((a[k] += a[k + i / 2]) >= MOD) a[k] -= MOD;
+            if ((a[k + i / 2] = u + MOD - a[k + i / 2]) >= MOD) a[k + i / 2] -= MOD;
+        }
+        for (int j = i, m = 1; j < n; j += i, ++m)
+            for (int k = j; k < j + i / 2; ++k) {
+                const uint u = a[k];
+                if ((a[k] += a[k + i / 2]) >= MOD) a[k] -= MOD;
+                a[k + i / 2] = (ull)(u + MOD - a[k + i / 2]) * root[m] % MOD;
+            }
+    }
+}
+
+int GetFFTSize(int n) {
+    int len = 1;
+    while (len < n) len *= 2;
+    return len;
+}
+
+void FFT(uint a[], int n, const uint root[]) { Butterfly(a, n, root); }
+
+void InvFFT(uint a[], int n, const uint root[]) {
+    InvButterfly(a, n, root);
+    const uint invn = InvMod(n);
+    for (int i = 0; i < n; ++i) a[i] = (ull)a[i] * invn % MOD;
+}
+
+std::vector<uint> FPSComp(std::vector<uint> f, std::vector<uint> g, int n) {
+    assert(empty(g) || g[0] == 0);
+    const int N = GetFFTSize(n);
+    std::vector<uint> root, inv_root;
+    tie(root, inv_root) = GetFFTRoot(N * 4);
+    // [y^(-1)] (f(y) / (-g(x) + y)) mod x^n in R[x]((y^(-1)))
+    const auto KinoshitaLi = [&root = as_const(root), &inv_root = as_const(inv_root)](
+                                 auto &&KinoshitaLi, std::vector<uint> &P, std::vector<uint> Q,
+                                 int d, int n) {
+        assert((int)size(P) == d * n * 2);
+        assert((int)size(Q) == d * n * 2);
+        if (n == 1) return;
+        Q.resize(d * n * 4);
+        Q[d * n * 2] = 1;
+        FFT(data(Q), d * n * 4, data(root));
+        if (n > 2) {
+            std::vector<uint> V(d * n * 2);
+            for (int i = 0; i < d * n * 4; i += 2) V[i / 2] = (ull)Q[i] * Q[i + 1] % MOD;
+            InvFFT(data(V), d * n * 2, data(inv_root));
+            assert(V[0] == 1);
+            V[0] = 0;
+            for (int i = 0; i < d * 2; ++i)
+                std::memset(data(V) + i * n + n / 2, 0, sizeof(uint) * (n / 2));
+            KinoshitaLi(KinoshitaLi, P, std::move(V), d * 2, n / 2);
+        }
+        FFT(data(P), d * n * 2, data(root));
+        for (int i = 0; i < d * n * 4; i += 2) {
+            const uint u = Q[i];
+            Q[i]         = (ull)P[i / 2] * Q[i + 1] % MOD;
+            Q[i + 1]     = (ull)P[i / 2] * u % MOD;
+        }
+        InvFFT(data(Q), d * n * 4, data(inv_root));
+        for (int i = 0; i < d; ++i) {
+            uint *const u = data(P) + i * n * 2;
+            std::memcpy(u, data(Q) + (i + d) * (n * 2), sizeof(uint) * n);
+            std::memset(u + n, 0, sizeof(uint) * n);
+        }
+    };
+    f.resize(N * 2);
+    g.resize(N * 2);
+    for (int i = N - 1; i >= 0; --i) f[i * 2] = f[i], f[i * 2 + 1] = 0;
+    for (int i = 0; i < N; ++i) g[i] = (g[i] != 0 ? MOD - g[i] : 0);
+    std::memset(data(g) + N, 0, sizeof(uint) * N);
+    KinoshitaLi(KinoshitaLi, f, std::move(g), 1, N);
+    f.resize(n);
+    return f;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n;
+    std::cin >> n;
+    std::vector<uint> f(n), g(n);
+    for (int i = 0; i < n; ++i) std::cin >> f[i];
+    for (int i = 0; i < n; ++i) std::cin >> g[i];
+    const auto fg = FPSComp(f, g, n);
+    for (int i = 0; i < n; ++i) std::cout << fg[i] << ' ';
+    return 0;
+}
