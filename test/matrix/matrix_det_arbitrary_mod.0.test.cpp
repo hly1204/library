aac490b100a944eaa3fdec38263fe485ef77c3c7
hly1204 <hly1204@qq.com>
2026-07-27 22:25:16 +0800

    minor

--- test/matrix/matrix_det_arbitrary_mod.0.test.cpp
+++ test/matrix/matrix_det_arbitrary_mod.0.test.cpp
@@ -1,5 +1,6 @@
 // competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/matrix_det_arbitrary_mod
 
+#include "xgcd.hpp"
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -18,34 +19,16 @@ int det(std::vector<std::vector<int>> A, int mod) {
             det = mod - det;
         }
         for (int j = i + 1; j < n; ++j) {
-            int Aii = A[i][i], Aji = A[j][i];
-            if (Aji == 0) continue;
-            // `a0` * `A[i][i]` + `a1` * `A[j][i]`     (1)
-            // `b0` * `A[i][i]` + `b1` * `A[j][i]`     (2)
-            // Use Euclidean algorithm to compute `a0, a1, b0, b1` s.t. (1) = 0 or (2) = 0
-            int a0 = 1, a1 = 0, b0 = 0, b1 = 1;
-            while (Aii != 0 && Aji != 0) {
-                if (Aii < Aji) {
-                    const int q = Aji / Aii;
-                    Aji -= Aii * q, b0 -= a0 * q, b1 -= a1 * q;
-                } else {
-                    const int q = Aii / Aji;
-                    Aii -= Aji * q, a0 -= b0 * q, a1 -= b1 * q;
-                }
-            }
-            if (a0 < mod) a0 += mod;
-            if (a1 < mod) a1 += mod;
-            if (b0 < mod) b0 += mod;
-            if (b1 < mod) b1 += mod;
+            if (A[j][i] == 0) continue;
+            const auto [s, t, d] = xgcd(A[i][i], A[j][i]);
+            const int a = -A[j][i] / d, b = A[i][i] / d;
+            // (row(i), row(j)) <- (s*row(i) + t*row(j), -A(j,i)/d*row(i) + A(i,i)/d*row(j))
             for (int k = i; k < n; ++k) {
                 const long long Aik = A[i][k], Ajk = A[j][k];
-                A[i][k] = (a0 * Aik + a1 * Ajk) % mod;
-                A[j][k] = (b0 * Aik + b1 * Ajk) % mod;
+                if ((A[i][k] = (s * Aik + t * Ajk) % mod) < 0) A[i][k] += mod;
+                if ((A[j][k] = (a * Aik + b * Ajk) % mod) < 0) A[j][k] += mod;
             }
-            if (Aii == 0) {
-                A[i].swap(A[j]);
-                det = mod - det;
-            }
+            // A(i,i) = d, A(j,i) = 0
         }
         if ((det = (long long)det * A[i][i] % mod) == 0) return 0;
     }
@@ -57,9 +40,9 @@ int main() {
     std::cin.tie(nullptr);
     int n, mod;
     std::cin >> n >> mod;
-    std::vector a(n, std::vector<int>(n));
+    std::vector A(n, std::vector<int>(n));
     for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j) std::cin >> a[i][j];
-    std::cout << det(a, mod) << '\n';
+        for (int j = 0; j < n; ++j) std::cin >> A[i][j];
+    std::cout << det(A, mod) << '\n';
     return 0;
 }
