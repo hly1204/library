9c3996b89e2cbc7184a39f278be626b0933bcf67
hly1204 <hly1204@qq.com>
2026-07-27 21:22:16 +0800

    minor

--- test/matrix/matrix_det_arbitrary_mod.0.test.cpp
+++ test/matrix/matrix_det_arbitrary_mod.0.test.cpp
@@ -4,33 +4,33 @@
 #include <iostream>
 #include <vector>
 
-int det(std::vector<std::vector<int>> a, int mod) {
+int det(std::vector<std::vector<int>> A, int mod) {
     assert(mod > 1);
-    const int n = a.size();
+    const int n = A.size();
     int det     = 1;
     for (int i = 0; i < n; ++i) {
         int pivot = i;
         for (; pivot < n; ++pivot)
-            if (a[pivot][i] != 0) break;
+            if (A[pivot][i] != 0) break;
         if (pivot == n) return 0;
         if (pivot != i) {
-            a[pivot].swap(a[i]);
+            A[pivot].swap(A[i]);
             det = mod - det;
         }
         for (int j = i + 1; j < n; ++j) {
-            int aii = a[i][i], aji = a[j][i];
-            if (aji == 0) continue;
-            // `a0` * `a[i][i]` + `a1` * `a[j][i]`     (1)
-            // `b0` * `a[i][i]` + `b1` * `a[j][i]`     (2)
+            int Aii = A[i][i], Aji = A[j][i];
+            if (Aji == 0) continue;
+            // `a0` * `A[i][i]` + `a1` * `A[j][i]`     (1)
+            // `b0` * `A[i][i]` + `b1` * `A[j][i]`     (2)
             // Use Euclidean algorithm to compute `a0, a1, b0, b1` s.t. (1) = 0 or (2) = 0
             int a0 = 1, a1 = 0, b0 = 0, b1 = 1;
-            while (aii != 0 && aji != 0) {
-                if (aii < aji) {
-                    const int q = aji / aii;
-                    aji -= aii * q, b0 -= a0 * q, b1 -= a1 * q;
+            while (Aii != 0 && Aji != 0) {
+                if (Aii < Aji) {
+                    const int q = Aji / Aii;
+                    Aji -= Aii * q, b0 -= a0 * q, b1 -= a1 * q;
                 } else {
-                    const int q = aii / aji;
-                    aii -= aji * q, a0 -= b0 * q, a1 -= b1 * q;
+                    const int q = Aii / Aji;
+                    Aii -= Aji * q, a0 -= b0 * q, a1 -= b1 * q;
                 }
             }
             if (a0 < mod) a0 += mod;
@@ -38,16 +38,16 @@ int det(std::vector<std::vector<int>> a, int mod) {
             if (b0 < mod) b0 += mod;
             if (b1 < mod) b1 += mod;
             for (int k = i; k < n; ++k) {
-                const long long aik = a[i][k], ajk = a[j][k];
-                a[i][k] = (aik * a0 + ajk * a1) % mod;
-                a[j][k] = (aik * b0 + ajk * b1) % mod;
+                const long long Aik = A[i][k], Ajk = A[j][k];
+                A[i][k] = (a0 * Aik + a1 * Ajk) % mod;
+                A[j][k] = (b0 * Aik + b1 * Ajk) % mod;
             }
-            if (aii == 0) {
-                a[i].swap(a[j]);
+            if (Aii == 0) {
+                A[i].swap(A[j]);
                 det = mod - det;
             }
         }
-        if ((det = (long long)det * a[i][i] % mod) == 0) return 0;
+        if ((det = (long long)det * A[i][i] % mod) == 0) return 0;
     }
     return det;
 }
